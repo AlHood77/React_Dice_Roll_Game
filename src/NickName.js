f27d8076e9d28b1d270361da5e9b4ea39c5a5c8b
Alan <alanhood77@gmail.com>
2020-07-28 18:21:49 +0100

    case switch statement added

---
+++ src/NickName.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NickName extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <p></p>
+        );
+    }
+}
+
+export default NickName;
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Die from './Die';
+import NickName from './NickName';
 import './RollDice.css'
 
 class RollDice extends Component {
@@ -11,7 +12,8 @@ class RollDice extends Component {
         this.state = { 
             die1: 'one',
             die2: 'one',
-            isRolling: false
+            isRolling: false,
+            
         };
         this.roll = this.roll.bind(this);
     }
@@ -25,8 +27,9 @@ class RollDice extends Component {
             Math.floor(Math.random() * this.props.sides.length)
         ];
 
+
         // set state
-        this.setState({ die1: newDie1, die2: newDie2, isRolling: true })
+        this.setState({ die1: newDie1, die2: newDie2, isRolling: true,  })
         
         // wait one second and set isRolling to false
         setTimeout(() => {
@@ -34,7 +37,62 @@ class RollDice extends Component {
         }, 1000);
     }
 
+    // nickname() {
+    //     if ((this.state.die1 === 'one') && (this.state.die2 === 'one')) {
+    //         return "Snake Eyes"
+    //     } else {
+    //         return "Sorry"
+    //     }
+            
+    // }
+
+
+    nickName() {
+        switch (true) {
+            case (this.state.die1 === 'one' && this.state.die2 === 'one'): return "Snake Eyes!"
+            case (this.state.die1 === 'one' && this.state.die2 === 'two'): return "Ace Caught a Deuce!"
+            case (this.state.die1 === 'one' && this.state.die2 === 'three'): return "Easy Four!"
+            case (this.state.die1 === 'one' && this.state.die2 === 'four'): return "Little Phoebe!"
+            case (this.state.die1 === 'one' && this.state.die2 === 'five'): return "Sixie From Dixie!"
+            case (this.state.die1 === 'one' && this.state.die2 === 'six'): return "The Devil!"
+            case (this.state.die1 === 'two' && this.state.die2 === 'one'): return "Australian Yo!"
+            case (this.state.die1 === 'two' && this.state.die2 === 'two'): return "Ballerina!"
+            case (this.state.die1 === 'two' && this.state.die2 === 'three'): return "Michael Jordan!"
+            case (this.state.die1 === 'two' && this.state.die2 === 'four'): return "Lumber Number!"
+            case (this.state.die1 === 'two' && this.state.die2 === 'five'): return "Skinny Dugan!"
+            case (this.state.die1 === 'two' && this.state.die2 === 'six'): return "Easy Eight!"
+            case (this.state.die1 === 'three' && this.state.die2 === 'one'): return "Little Joe From Kokomos!"
+            case (this.state.die1 === 'three' && this.state.die2 === 'two'): return "The Fever!"
+            case (this.state.die1 === 'three' && this.state.die2 === 'three'): return "Brooklyn Forest!"
+            case (this.state.die1 === 'three' && this.state.die2 === 'four'): return "Skinny McKinney!"
+            case (this.state.die1 === 'three' && this.state.die2 === 'five'): return "Easy Eight!"
+            case (this.state.die1 === 'three' && this.state.die2 === 'six'): return "Lou Brown!"
+            case (this.state.die1 === 'four' && this.state.die2 === 'one'): return "No Field Five!"
+            case (this.state.die1 === 'four' && this.state.die2 === 'two'): return "Jimmie Hicks!"
+            case (this.state.die1 === 'four' && this.state.die2 === 'three'): return "Big Red!"
+            case (this.state.die1 === 'four' && this.state.die2 === 'four'): return "Square Pair!"
+            case (this.state.die1 === 'four' && this.state.die2 === 'five'): return "Jesse James!"
+            case (this.state.die1 === 'four' && this.state.die2 === 'six'): return "Tennessee!"
+            case (this.state.die1 === 'five' && this.state.die2 === 'one'): return "Easy Six!"
+            case (this.state.die1 === 'five' && this.state.die2 === 'two'): return "Benny Blue!"
+            case (this.state.die1 === 'five' && this.state.die2 === 'three'): return "Eighter from Decatur!"
+            case (this.state.die1 === 'five' && this.state.die2 === 'four'): return "Railroad Nine!"
+            case (this.state.die1 === 'five' && this.state.die2 === 'five'): return "Puppy Paws!"
+            case (this.state.die1 === 'five' && this.state.die2 === 'six'): return "Six Five No Jive!"
+            case (this.state.die1 === 'six' && this.state.die2 === 'one'): return "Six One You're Done!"
+            case (this.state.die1 === 'six' && this.state.die2 === 'two'): return "Easy Eight!"
+            case (this.state.die1 === 'six' && this.state.die2 === 'three'): return "Nina From Passadena!"
+            case (this.state.die1 === 'six' && this.state.die2 === 'four'): return "Big One on the End!"
+            case (this.state.die1 === 'six' && this.state.die2 === 'five'): return "Yo!"
+            case (this.state.die1 === 'six' && this.state.die2 === 'six'): return "Midnight!"
+            default: return "No match"
+        }
+    }
+    
+
     render() {
+
+
         return (
             <div className="RollDice">  
                 <div className="RollDice-die-container">
@@ -45,6 +103,7 @@ class RollDice extends Component {
                 <button onClick={this.roll} disabled={this.state.isRolling}>
                     {this.state.isRolling ? 'Rolling...' : 'Roll Dice!'}
                 </button>
+                <p>{this.nickName()}</p>
             </div>
            
         );
