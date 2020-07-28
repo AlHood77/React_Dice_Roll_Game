ccb613f05be40489189fd8c54315ea4d27e95937
Alan <alanhood77@gmail.com>
2020-07-28 13:59:51 +0100

    die and rollDice components added

---
+++ src/Die.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Die.css'
+
+class Die extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <i className={`Die fas fa-dice-${this.props.face}`} />
+            
+        );
+    }
+}
+
+export default Die;
